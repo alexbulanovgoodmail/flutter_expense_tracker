5d007f6fdae2f04c4f66a9a5eabc811d9bbd6423
Александр Буланов <alexbulanovgoodmail@gmail.com>
2026-01-27 10:52:17 +0300

    Добавлены форматтеры для даты и валюты, карта иконок для категорий расходов в модель Expense

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,8 +1,27 @@
 import 'package:uuid/uuid.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter/material.dart';
 
 const Uuid uuid = Uuid();
 
 enum Category { food, travel, entertainment, utilities, leisure, work, others }
+
+const categoryIcons = {
+  Category.food: Icons.restaurant,
+  Category.travel: Icons.flight,
+  Category.entertainment: Icons.movie,
+  Category.utilities: Icons.lightbulb,
+  Category.leisure: Icons.beach_access,
+  Category.work: Icons.work,
+  Category.others: Icons.shopping_bag,
+};
+
+final dateFormatter = DateFormat('dd.MM.yyyy', 'ru_RU');
+final currencyFormatter = NumberFormat.currency(
+  locale: 'en_US',
+  symbol: '\$',
+  decimalDigits: 2,
+);
 
 class Expense {
   Expense({
@@ -17,4 +36,7 @@ class Expense {
   final double amount;
   final DateTime date;
   final Category category;
+
+  String get formattedDate => dateFormatter.format(date);
+  String get formattedCurrency => currencyFormatter.format(amount);
 }
