4c14328e43f9535e4155bb8fff95313b249a0b63
Александр Буланов <alexbulanovgoodmail@gmail.com>
2026-01-27 10:53:01 +0300

    Создан виджет ExpensesItem с карточкой для отображения расхода

---
+++ lib/widgets/expenses_item.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_expense_tracker/models/expense.dart';
+
+class ExpensesItem extends StatelessWidget {
+  const ExpensesItem({super.key, required this.expense});
+
+  final Expense expense;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(expense.title),
+            const SizedBox(height: 4.0),
+            Row(
+              children: <Widget>[
+                Text(
+                  expense.formattedCurrency,
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                const Spacer(),
+                Row(
+                  children: <Widget>[
+                    Icon(categoryIcons[expense.category]),
+                    const SizedBox(width: 8.0),
+                    Text(expense.formattedDate),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
