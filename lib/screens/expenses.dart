e007dbd37db5e5868616f7505191b7cb93b1351d
Александр Буланов <alexbulanovgoodmail@gmail.com>
2026-01-27 10:31:30 +0300

    Добавлены фиктивные данные, отображение списка расходов

--- lib/screens/expenses.dart
+++ lib/screens/expenses.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_expense_tracker/models/expense.dart';
+import 'package:flutter_expense_tracker/widgets/expenses_list.dart';
 
 class ExpensesScreen extends StatefulWidget {
   const ExpensesScreen({super.key});
@@ -8,18 +10,35 @@ class ExpensesScreen extends StatefulWidget {
 }
 
 class _ExpensesScreenState extends State<ExpensesScreen> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Groceries',
+      amount: 50.0,
+      date: DateTime.now(),
+      category: Category.food,
+    ),
+    Expense(
+      title: 'Movie Tickets',
+      amount: 30.0,
+      date: DateTime.now(),
+      category: Category.entertainment,
+    ),
+    Expense(
+      title: 'Electricity Bill',
+      amount: 75.0,
+      date: DateTime.now(),
+      category: Category.utilities,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Expenses')),
       body: Column(
         children: [
-          Center(
-            child: Text(
-              'Welcome to the Expenses Screen!',
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
+          const Text('The chart!'),
+          Expanded(child: ExpensesList(expenses: _registeredExpenses)),
         ],
       ),
     );
---
+++ lib/widgets/expenses_list.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_expense_tracker/models/expense.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList({super.key, required this.expenses});
+
+  final List<Expense> expenses;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(
+            '${expenses[index].title} - \$${expenses[index].amount.toStringAsFixed(2)}',
+          ),
+        );
+      },
+    );
+  }
+}
